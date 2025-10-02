7f2f2c0b7bd4deaa0d193bc194cf74b82a95566e
Md Bocktear Abid Borat <boratabid@gmail.com>
2025-10-02 16:19:39 +0600

    Commit-1

---
+++ email-password-auth/src/Components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      
+      <h3 className='text-4xl'>Welcome to email password auth home</h3>
+    </div>
+  );
+};
+
+export default Home;
